eb9cd2b29818575d459b532521aed87841ce72b4
= <ayoubelmahfoudi05@gmail.com>
2023-12-13 08:34:05 +0100

    Solution submitted on the platform, and tests passed successfully.

--- collatz-conjecture/collatz_conjecture.cpp
+++ collatz-conjecture/collatz_conjecture.cpp
@@ -1,5 +1,21 @@
 #include "collatz_conjecture.h"
+#include <stdexcept>
 
 namespace collatz_conjecture {
-
-}  // namespace collatz_conjecture
+int steps(int number) {
+  int counter = 0;
+  if (number < 1) {
+    throw std::domain_error("invalid_argument");
+  }
+  while (number > 1) {
+    if (number % 2 == 0) {
+      number /= 2;
+      counter++;
+    } else {
+      number = 3 * number + 1;
+      counter++;
+    }
+  }
+  return counter;
+}
+} // namespace collatz_conjecture
--- collatz-conjecture/collatz_conjecture.h
+++ collatz-conjecture/collatz_conjecture.h
@@ -2,7 +2,7 @@
 #define COLLATZ_CONJECTURE_H
 
 namespace collatz_conjecture {
-
-}  // namespace collatz_conjecture
+int steps(int number);
+} // namespace collatz_conjecture
 
 #endif // COLLATZ_CONJECTURE_H
