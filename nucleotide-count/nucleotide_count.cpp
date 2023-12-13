f88e4091af79878c65bd4aeac369c19cf92bbb2c
= <ayoubelmahfoudi05@gmail.com>
2023-12-13 08:32:27 +0100

    Solution submitted on the platform, and tests passed successfully.

--- nucleotide-count/nucleotide_count.cpp
+++ nucleotide-count/nucleotide_count.cpp
@@ -1,5 +1,24 @@
 #include "nucleotide_count.h"
+#include <map>
+#include <stdexcept>
 
 namespace nucleotide_count {
+std::map<char, int> count(std::string DNA) {
+  std::map<char, int> result({{'A', 0}, {'C', 0}, {'G', 0}, {'T', 0}});
+  for (char ch : DNA) {
+    if (ch == 'A') {
+      result[ch]++;
+    } else if (ch == 'C') {
+      result[ch]++;
+    } else if (ch == 'G') {
+      result[ch]++;
+    } else if (ch == 'T') {
+      result[ch]++;
+    } else {
+      throw std::invalid_argument("Accept only A C G T.");
+    }
+  }
+  return result;
+}
 
-}  // namespace nucleotide_count
+} // namespace nucleotide_count
--- nucleotide-count/nucleotide_count.h
+++ nucleotide-count/nucleotide_count.h
@@ -1,8 +1,12 @@
+#include <map>
+#include <string>
 #if !defined(NUCLEOTIDE_COUNT_H)
 #define NUCLEOTIDE_COUNT_H
 
 namespace nucleotide_count {
 
-}  // namespace nucleotide_count
+std::map<char, int> count(std::string DNA);
+
+} // namespace nucleotide_count
 
 #endif // NUCLEOTIDE_COUNT_H
