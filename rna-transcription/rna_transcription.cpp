768b08927a542189ec1d04b5a2bb8bdf9b674221
= <ayoubelmahfoudi05@gmail.com>
2023-12-13 08:33:33 +0100

    Solution submitted on the platform, and tests passed successfully.

--- rna-transcription/rna_transcription.cpp
+++ rna-transcription/rna_transcription.cpp
@@ -1,5 +1,32 @@
 #include "rna_transcription.h"
+#include <stdexcept>
+#include <string>
 
 namespace rna_transcription {
-
-}  // namespace rna_transcription
+char to_rna(char DNA) {
+  switch (DNA) {
+  case 'A':
+    return 'U';
+    break;
+  case 'C':
+    return 'G';
+    break;
+  case 'G':
+    return 'C';
+    break;
+  case 'T':
+    return 'A';
+    break;
+  default:
+    throw std::invalid_argument("Bad base DNA");
+    break;
+  }
+}
+std::string to_rna(std::string DNA) {
+  std::string RNA;
+  for (char ch : DNA) {
+    RNA += rna_transcription::to_rna(ch);
+  }
+  return RNA;
+}
+} // namespace rna_transcription
--- rna-transcription/rna_transcription.h
+++ rna-transcription/rna_transcription.h
@@ -1,8 +1,10 @@
+#include <string>
 #if !defined(RNA_TRANSCRIPTION_H)
 #define RNA_TRANSCRIPTION_H
 
 namespace rna_transcription {
-
-}  // namespace rna_transcription
+char to_rna(char DNA);
+std::string to_rna(std::string DNA);
+} // namespace rna_transcription
 
 #endif // RNA_TRANSCRIPTION_H
