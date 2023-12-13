0b41d8b3c44ac7e678486a7b11f5e972f48246bd
= <ayoubelmahfoudi05@gmail.com>
2023-12-13 08:28:14 +0100

    Solution submitted on the platform and tests passed successfully.

--- grade-school/grade_school.cpp
+++ grade-school/grade_school.cpp
@@ -1,5 +1,24 @@
 #include "grade_school.h"
 
 namespace grade_school {
+void school::add(const std::string &name, int grade) {
+  if (rosterSt.find(grade) == rosterSt.end()) {
+    rosterSt[grade] = std::vector<std::string>();
+  }
+  rosterSt[grade].push_back(name);
+  // Optionally, you can sort the vector after each addition
+  std::sort(rosterSt[grade].begin(), rosterSt[grade].end());
+}
 
-}  // namespace grade_school
+std::map<int, std::vector<std::string>> school::roster() const {
+  return rosterSt;
+}
+
+std::vector<std::string> school::grade(int grade) const {
+  if (rosterSt.find(grade) == rosterSt.end()) {
+    return std::vector<std::string>(); // Return an empty vector if the grade
+                                       // doesn't exist
+  }
+  return rosterSt.at(grade);
+}
+} // namespace grade_school
--- grade-school/grade_school.h
+++ grade-school/grade_school.h
@@ -1,8 +1,22 @@
-#if !defined(GRADE_SCHOOL_H)
+#ifndef GRADE_SCHOOL_H
 #define GRADE_SCHOOL_H
 
-namespace grade_school {
+#include <algorithm>
+#include <map>
+#include <set>
+#include <string>
+#include <vector>
 
-}  // namespace grade_school
+namespace grade_school {
+class school {
+private:
+  std::map<int, std::vector<std::string>> rosterSt;
+
+public:
+  void add(const std::string &name, int grade);
+  std::map<int, std::vector<std::string>> roster() const;
+  std::vector<std::string> grade(int grade) const;
+};
+} // namespace grade_school
 
 #endif // GRADE_SCHOOL_H
