64001ecce7382b86fccb31ff400b119622e22a69
= <ayoubelmahfoudi05@gmail.com>
2023-12-13 08:26:30 +0100

    Solution submitted on the platform and tests passed successfully.

--- two-fer/two_fer.cpp
+++ two-fer/two_fer.cpp
@@ -1,7 +1,9 @@
 #include "two_fer.h"
+#include <string>
 
-namespace two_fer
-{
-
+namespace two_fer {
+const std::string two_fer() { return "One for you, one for me."; }
+const std::string two_fer(std::string name) {
+  return "One for " + name + ", one for me.";
+}
 } // namespace two_fer
-
--- two-fer/two_fer.h
+++ two-fer/two_fer.h
@@ -1,9 +1,10 @@
 #if !defined(TWO_FER_H)
 #define TWO_FER_H
+#include <string>
 
-namespace two_fer
-{
-
+namespace two_fer {
+const std::string two_fer();
+const std::string two_fer(std::string name);
 } // namespace two_fer
 
-#endif //TWO_FER_H
+#endif // TWO_FER_H
