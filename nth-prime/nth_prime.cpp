a0040cf670ece7b265f3aa2629682512d889efc7
= <ayoubelmahfoudi05@gmail.com>
2023-12-14 12:56:21 +0100

    Solution submitted onthe platform and tests passed successfully (Sieve of Eratosthenes algorithm was used to determine prime number.)

--- nth-prime/nth_prime.cpp
+++ nth-prime/nth_prime.cpp
@@ -1,5 +1,31 @@
 #include "nth_prime.h"
+#include <cstring>
+
+std::vector<int> primes;
+bool ret = false;
 
 namespace nth_prime {
-
-}  // namespace nth_prime
+int nth(int num) {
+  if (ret == false) {
+    GetPrimes();
+    ret = true;
+  }
+  return primes[num + 1];
+}
+void GetPrimes() {
+  bool IsPrime[MAX_SIZE];
+  memset(IsPrime, true, sizeof(IsPrime));
+  for (int p = 2; p * p < MAX_SIZE; p++) {
+    if (IsPrime[p] == true) {
+      for (int i = p * p; i < MAX_SIZE; i += p) {
+        IsPrime[i] = false;
+      }
+    }
+  }
+  for (int p = 2; p < MAX_SIZE; p++) {
+    if (IsPrime[p]) {
+      primes.push_back(p);
+    }
+  }
+}
+} // namespace nth_prime
--- nth-prime/nth_prime.h
+++ nth-prime/nth_prime.h
@@ -1,7 +1,10 @@
 #if !defined(NTH_PRIME_H)
 #define NTH_PRIME_H
+#include <vector>
+#define MAX_SIZE 1000000
 
 namespace nth_prime {
+void GetPrimes();
 int nth(int num);
 } // namespace nth_prime
 
