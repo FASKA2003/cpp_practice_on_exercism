aae54ebb9700ea944a2f9b978b636cf5a030042b
= <ayoubelmahfoudi05@gmail.com>
2023-12-16 08:18:51 +0100

    First attempt, tests doesn't passed successfully.

--- queen-attack/queen_attack.cpp
+++ queen-attack/queen_attack.cpp
@@ -1,5 +1,19 @@
 #include "queen_attack.h"
+#include <utility>
 
 namespace queen_attack {
-
-}  // namespace queen_attack
+bool chess_board::can_attack() const {
+  if (white().first <= 0 || white().second <= 0 || black().first <= 0 ||
+      black().second <= 0) {
+    throw std::domain_error("Invalid argument");
+  }
+  if (white().first == black().first || white().second == black().second) {
+    return true;
+  }
+  if (std::abs(white().first - black().first) ==
+      std::abs(white().second - black().second)) {
+    return true;
+  }
+  return false;
+}
+} // namespace queen_attack
--- queen-attack/queen_attack.h
+++ queen-attack/queen_attack.h
@@ -1,8 +1,21 @@
+#include <stdexcept>
+#include <utility>
+#include <valarray>
 #if !defined(QUEEN_ATTACK_H)
 #define QUEEN_ATTACK_H
 
 namespace queen_attack {
-
-}  // namespace queen_attack
+class chess_board {
+public:
+  std::pair<int, int> white_;
+  std::pair<int, int> black_;
+  chess_board(const std::pair<int, int> &white,
+              const std::pair<int, int> &black)
+      : white_(white), black_(black) {}
+  std::pair<int, int> white() const { return white_; }
+  std::pair<int, int> black() const { return black_; }
+  bool can_attack() const;
+};
+} // namespace queen_attack
 
 #endif // QUEEN_ATTACK_H
