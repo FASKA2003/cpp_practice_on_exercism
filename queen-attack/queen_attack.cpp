ec257830ae7d38c3d6558ad889bab97b2cacd446
= <ayoubelmahfoudi05@gmail.com>
2023-12-16 23:50:26 +0100

    Second submition tests doesn't passed.

--- queen-attack/queen_attack.cpp
+++ queen-attack/queen_attack.cpp
@@ -2,11 +2,20 @@
 #include <utility>
 
 namespace queen_attack {
-bool chess_board::can_attack() const {
-  if (white().first <= 0 || white().second <= 0 || black().first <= 0 ||
-      black().second <= 0) {
+chess_board::chess_board(const std::pair<int, int> &white,
+                         const std::pair<int, int> &black) {
+  if (white.first <= 0 || white.second <= 0 || black.first <= 0 ||
+      black.second <= 0) {
     throw std::domain_error("Invalid argument");
+  } else {
+    this->white_.first = white.first;
+    this->white_.second = white.second;
+    this->black_.first = black.first;
+    this->black_.second = black.second;
   }
+}
+
+bool chess_board::can_attack() const {
   if (white().first == black().first || white().second == black().second) {
     return true;
   }
--- queen-attack/queen_attack.h
+++ queen-attack/queen_attack.h
@@ -10,8 +10,7 @@ public:
   std::pair<int, int> white_;
   std::pair<int, int> black_;
   chess_board(const std::pair<int, int> &white,
-              const std::pair<int, int> &black)
-      : white_(white), black_(black) {}
+              const std::pair<int, int> &black);
   std::pair<int, int> white() const { return white_; }
   std::pair<int, int> black() const { return black_; }
   bool can_attack() const;
