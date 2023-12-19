53d35dcbc37af35c3cef274e31bf4aebc59ab7e1
= <ayoubelmahfoudi05@gmail.com>
2023-12-19 13:26:31 +0100

    Proble solved, solution submitted on the platform.

--- secret-handshake/secret_handshake.cpp
+++ secret-handshake/secret_handshake.cpp
@@ -1,5 +1,28 @@
 #include "secret_handshake.h"
+#include <algorithm>
+#include <bitset>
+#include <ratio>
+#include <string>
 
 namespace secret_handshake {
-
-}  // namespace secret_handshake
+std::vector<std::string> commands(int n) {
+  std::vector<std::string> result;
+  std::bitset<5> decimalBitset(n);
+  if (decimalBitset.to_ulong() & 1) {
+    result.push_back("wink");
+  }
+  if ((decimalBitset >> 1).to_ulong() & 1) {
+    result.push_back("double blink");
+  }
+  if ((decimalBitset >> 2).to_ulong() & 1) {
+    result.push_back("close your eyes");
+  }
+  if ((decimalBitset >> 3).to_ulong() & 1) {
+    result.push_back("jump");
+  }
+  if ((decimalBitset >> 4).to_ulong() & 1) {
+    std::reverse(result.begin(), result.end());
+  }
+  return result;
+}
+} // namespace secret_handshake
--- secret-handshake/secret_handshake.h
+++ secret-handshake/secret_handshake.h
@@ -1,8 +1,11 @@
 #if !defined(SECRET_HANDSHAKE_H)
 #define SECRET_HANDSHAKE_H
+#include <bitset>
+#include <string>
+#include <vector>
 
 namespace secret_handshake {
-
-}  // namespace secret_handshake
+std::vector<std::string> commands(int number);
+} // namespace secret_handshake
 
 #endif // SECRET_HANDSHAKE_H
