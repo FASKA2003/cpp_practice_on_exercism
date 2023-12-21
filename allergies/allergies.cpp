7652c5871fb6e54c49e1a3398eb9c0b71348568f
= <ayoubelmahfoudi05@gmail.com>
2023-12-21 19:28:18 +0100

    Start working on the allergies problem.

---
+++ allergies/allergies.cpp
@@ -0,0 +1,26 @@
+#include "allergies.h"
+#include <string>
+std::string allergies_table[8] = {"eggs",         "peanuts",  "shellfish",
+                                  "strawberries", "tomatoes", "chocolate",
+                                  "pollen",       "cats"};
+
+namespace allergies {
+
+allergy_test::allergy_test(int score_N) { this->score = score_N; }
+std::unordered_set<std::string> allergy_test::get_allergies() {
+  std::unordered_set<std::string> result;
+  for (int i = 0; i < 8; i++) {
+    if ((score >> i) & 1) {
+      result.insert(allergies_table[i]);
+    }
+  }
+  return result;
+}
+bool allergy_test::is_allergic(const std::string allergy) {
+  std::unordered_set<std::string> result = get_allergies();
+  if (result.find(allergy) == result.end()) {
+    return false;
+  }
+  return true;
+}
+} // namespace allergies
---
+++ allergies/allergies.h
@@ -0,0 +1,16 @@
+#if !defined(ALLERGIES_H)
+#define ALLERGIES_H
+#include <string>
+#include <unordered_set>
+
+namespace allergies {
+class allergy_test {
+public:
+  int score;
+  allergy_test(int score_N);
+  std::unordered_set<std::string> get_allergies();
+  bool is_allergic(std::string allergy);
+};
+} // namespace allergies
+
+#endif // ALLERGIES_H
