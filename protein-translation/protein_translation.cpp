48f98876961ddc1a1a9b0a1aa64e6e9746cf5929
= <ayoubelmahfoudi05@gmail.com>
2023-12-22 15:13:44 +0100

    Solution submitted and tests passed successfully for the protein translation problem.

--- protein-translation/protein_translation.cpp
+++ protein-translation/protein_translation.cpp
@@ -1,5 +1,26 @@
 #include "protein_translation.h"
 
-namespace protein_translation {
+std::unordered_map<std::string, std::string> codonToProtein = {
+    {"AUG", "Methionine"}, {"UUU", "Phenylalanine"}, {"UUC", "Phenylalanine"},
+    {"UUA", "Leucine"},    {"UUG", "Leucine"},       {"UCU", "Serine"},
+    {"UCC", "Serine"},     {"UCA", "Serine"},        {"UCG", "Serine"},
+    {"UAU", "Tyrosine"},   {"UAC", "Tyrosine"},      {"UGU", "Cysteine"},
+    {"UGC", "Cysteine"},   {"UGG", "Tryptophan"},    {"UAA", "STOP"},
+    {"UAG", "STOP"},       {"UGA", "STOP"}};
 
-}  // namespace protein_translation
+namespace protein_translation {
+std::vector<std::string> proteins(std::string RNA) {
+  std::vector<std::string> result;
+  for (int i = 0; i < RNA.length(); i += 3) {
+    std::string codon = RNA.substr(i, 3);
+    auto it = codonToProtein.find(codon);
+    if (it != codonToProtein.end()) {
+      if (it->second == "STOP") {
+        break;
+      }
+      result.push_back(it->second);
+    }
+  }
+  return result;
+}
+} // namespace protein_translation
--- protein-translation/protein_translation.h
+++ protein-translation/protein_translation.h
@@ -1,8 +1,13 @@
+#include <unordered_map>
 #if !defined(PROTEIN_TRANSLATION_H)
 #define PROTEIN_TRANSLATION_H
+#include <map>
+#include <set>
+#include <string>
+#include <vector>
 
 namespace protein_translation {
-
-}  // namespace protein_translation
+std::vector<std::string> proteins(std::string RNA);
+} // namespace protein_translation
 
 #endif // PROTEIN_TRANSLATION_H
