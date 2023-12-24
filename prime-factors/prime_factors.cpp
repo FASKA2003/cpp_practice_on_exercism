30d71c9137cae941f99134df2daa8684080561c7
= <ayoubelmahfoudi05@gmail.com>
2023-12-24 11:56:23 +0100

    Start working on the prime factors problem.

---
+++ prime-factors/prime_factors.cpp
@@ -0,0 +1,10 @@
+#include "prime_factors.h"
+#include <cstring>
+#include <vector>
+
+namespace prime_factors {
+std::vector<int> of(unsigned int n) {
+  std::vector<int> primes;
+  return primes;
+}
+} // namespace prime_factors
