dd0de7a184e20ec67e1c83d8b9e82d7cfa1da04d
= <ayoubelmahfoudi05@gmail.com>
2024-01-04 08:22:23 +0100

    Solution submitted and tests passed successfully.

--- clock/clock.cpp
+++ clock/clock.cpp
@@ -1,5 +1,34 @@
 #include "clock.h"
+#include <iomanip>
+#include <sstream>
 
 namespace date_independent {
+clock clock::at(int hours, int minutes) {
+  constexpr int HOURS_IN_DAY{24};
+  constexpr int MINUTES_IN_HOUR{60};
+  constexpr int MINUTES_IN_DAY{HOURS_IN_DAY * MINUTES_IN_HOUR};
 
-}  // namespace date_independent
+  int total_min{minutes + hours * MINUTES_IN_HOUR};
+  total_min = (total_min % MINUTES_IN_DAY + MINUTES_IN_DAY) % MINUTES_IN_DAY;
+  minutes = total_min % MINUTES_IN_HOUR;
+  hours = total_min / MINUTES_IN_HOUR;
+
+  return clock{hours, minutes};
+}
+clock::operator std::string() const {
+  std::stringstream ss;
+  ss << std::setfill('0') << std::setw(2) << this->m_hours << ':'
+     << std::setw(2) << this->m_minutes;
+
+  return ss.str();
+}
+clock clock::plus(int minutes) {
+  return clock::at(this->m_hours, this->m_minutes + minutes);
+}
+bool clock::operator==(clock c) const {
+  return this->m_hours == c.m_hours && this->m_minutes == c.m_minutes;
+}
+bool clock::operator!=(clock c) const {
+  return this->m_hours != c.m_hours || this->m_minutes != c.m_minutes;
+}
+} // namespace date_independent
--- clock/clock.h
+++ clock/clock.h
@@ -1,8 +1,22 @@
+#include <ctime>
 #if !defined(CLOCK_H)
 #define CLOCK_H
+#include <string>
 
 namespace date_independent {
+class clock {
+  int m_minutes;
+  int m_hours;
 
-}  // namespace date_independent
+  clock(int hours, int minutes) : m_hours{hours}, m_minutes{hours} {}
+
+public:
+  static clock at(int hours, int minutes);
+  operator std::string() const;
+  clock plus(int minutes);
+  bool operator==(clock c) const;
+  bool operator!=(clock c) const;
+};
+} // namespace date_independent
 
 #endif // CLOCK_H
