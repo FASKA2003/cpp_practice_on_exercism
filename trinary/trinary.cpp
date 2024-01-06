1d8a1c35fbb4a45b9b3818f76c069b8b90e08b0f
= <ayoubelmahfoudi05@gmail.com>
2024-01-06 14:45:13 +0100

    trinary conversion : Tests passed successfully for the proposed solution.

--- trinary/trinary.cpp
+++ trinary/trinary.cpp
@@ -1,5 +1,16 @@
 #include "trinary.h"
 
 namespace trinary {
-
+  int to_decimal(std::string input) {
+    int length = input.length();
+    int result = 0, exp,val;
+    for (int i = 0; i < length; i++) {
+      val = input.at(i) - '0';
+      exp = length - i - 1;
+      if (val <= 2) {
+        result += (int)(val * pow(3.0,exp)); 
+      }
+    }
+    return result;
+  }
 }  // namespace trinary
--- trinary/trinary.h
+++ trinary/trinary.h
@@ -1,8 +1,10 @@
 #if !defined(TRINARY_H)
 #define TRINARY_H
+#include <cmath>
+#include <string>
 
 namespace trinary {
-
-}  // namespace trinary
+int to_decimal(std::string input);
+} // namespace trinary
 
 #endif // TRINARY_H
