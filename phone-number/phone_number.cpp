1493beaf5640783cf8b2c47c388b4acfbdff9553
= <ayoubelmahfoudi05@gmail.com>
2024-01-07 19:46:33 +0100

    Phone number problem solved using regex, tests passed successfully.

--- phone-number/phone_number.cpp
+++ phone-number/phone_number.cpp
@@ -1,5 +1,22 @@
 #include "phone_number.h"
+#include <regex>
+#include <stdexcept>
+#include <string>
 
 namespace phone_number {
+phone_number::phone_number(const std::string &s) {
+  std::regex re{R"(^\D*(?:\+?1)?\D*([2-9]\d{2})\D*([2-9]\d{2})\D*(\d{4})\D*$)"};
+  std::smatch sm;
 
-}  // namespace phone_number
+  if (std::regex_match(s, sm, re)) {
+    inner_number = sm[1].str() + sm[2].str() + sm[3].str();
+    inner_area_code = sm[1].str();
+    inner_string =
+        "(" + sm[1].str() + ")" + " " + sm[2].str() + "-" + sm[3].str();
+  } else {
+    throw std::domain_error("invalid input");
+  }
+}
+std::string phone_number::number() const { return inner_number; }
+std::string phone_number::area_code() const { return inner_area_code; }
+} // namespace phone_number
--- phone-number/phone_number.h
+++ phone-number/phone_number.h
@@ -1,8 +1,19 @@
 #if !defined(PHONE_NUMBER_H)
 #define PHONE_NUMBER_H
+#include <regex>
+#include <string>
 
 namespace phone_number {
+class phone_number {
+  std::string inner_string, inner_number, inner_area_code;
 
-}  // namespace phone_number
+public:
+  phone_number(const std::string &);
+
+  std::string number() const;
+  std::string area_code() const;
+  operator std::string() const { return inner_string; }
+};
+} // namespace phone_number
 
 #endif // PHONE_NUMBER_H
