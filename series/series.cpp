720482f42d8dc325fc8635177443b1e051cc8a5b
= <ayoubelmahfoudi05@gmail.com>
2024-01-08 13:34:26 +0100

    Solution submitted, Tests passed successfully.

--- series/series.cpp
+++ series/series.cpp
@@ -1,5 +1,15 @@
 #include "series.h"
 
 namespace series {
-
-}  // namespace series
+std::vector<std::string> slice(std::string number, int len) {
+  std::vector<std::string> result;
+  int strLen = number.length();
+  if ((strLen < len) || (len <= 0) || (number.empty())) {
+    throw std::domain_error("Invalid input.");
+  }
+  for (int i = 0; i + len <= strLen; i++) {
+    result.push_back(number.substr(i, len));
+  }
+  return result;
+}
+} // namespace series
--- series/series.h
+++ series/series.h
@@ -1,8 +1,11 @@
 #if !defined(SERIES_H)
 #define SERIES_H
+#include <stdexcept>
+#include <string>
+#include <vector>
 
 namespace series {
-
-}  // namespace series
+std::vector<std::string> slice(std::string number, int length);
+} // namespace series
 
 #endif // SERIES_H
