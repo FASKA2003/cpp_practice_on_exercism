23191c87b06870207ed7a9819f443e9347a2ee67
= <ayoubelmahfoudi05@gmail.com>
2024-01-09 15:26:34 +0100

    implementation done, tests passed successfully.

--- complex-numbers/complex_numbers.cpp
+++ complex-numbers/complex_numbers.cpp
@@ -1,5 +1,27 @@
 #include "complex_numbers.h"
 
 namespace complex_numbers {
-
-}  // namespace complex_numbers
+double Complex::real() { return this->realN; }
+double Complex::imag() { return this->imagN; }
+double Complex::abs() { return sqrt(pow(realN, 2) + pow(imagN, 2)); }
+Complex Complex::conj() { return Complex(realN, -1 * imagN); }
+Complex Complex::exp() {
+  return Complex(pow(M_E, realN) * cos(imagN), pow(M_E, realN) * sin(imagN));
+}
+Complex Complex::operator+(Complex const &num) const {
+  return Complex(realN + num.realN, imagN + num.imagN);
+}
+Complex Complex::operator-(Complex const &num) const {
+  return Complex(realN - num.realN, imagN - num.imagN);
+}
+Complex Complex::operator*(Complex const &num) const {
+  return Complex(realN * num.realN - imagN * num.imagN,
+                 imagN * num.realN + realN * num.imagN);
+}
+Complex Complex::operator/(Complex const &num) const {
+  double den = pow(num.realN, 2) + pow(num.imagN, 2);
+  double nom1 = realN * num.realN + imagN * num.imagN;
+  double nom2 = imagN * num.realN - realN * num.imagN;
+  return Complex(nom1 / den, nom2 / den);
+}
+} // namespace complex_numbers
--- complex-numbers/complex_numbers.h
+++ complex-numbers/complex_numbers.h
@@ -1,8 +1,28 @@
 #if !defined(COMPLEX_NUMBERS_H)
 #define COMPLEX_NUMBERS_H
+#include <cmath>
+#define M_PI 3.14159265358979323846
 
 namespace complex_numbers {
+class Complex {
+  double realN, imagN;
 
-}  // namespace complex_numbers
+public:
+  Complex(double r = 0, double i = 0) {
+    realN = r;
+    imagN = i;
+  }
+  double real();
+  double imag();
+  double abs();
+  Complex conj();
+  Complex exp();
+  Complex operator+(Complex const &num) const;
+  Complex operator-(Complex const &num) const;
+  Complex operator*(Complex const &num) const;
+  Complex operator/(Complex const &num) const;
+};
 
-#endif  // COMPLEX_NUMBERS_H
+} // namespace complex_numbers
+
+#endif // COMPLEX_NUMBERS_H
