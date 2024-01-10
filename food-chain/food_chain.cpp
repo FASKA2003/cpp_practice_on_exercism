20119571ec78a2a41a58b4d6f1bc9d7edd81966f
= <ayoubelmahfoudi05@gmail.com>
2024-01-10 16:33:40 +0100

    Solution submitted, tests passed successfully.

--- food-chain/food_chain.cpp
+++ food-chain/food_chain.cpp
@@ -1,5 +1,48 @@
 #include "food_chain.h"
 
-namespace food_chain {
+std::string start_intro = {"I know an old lady who swallowed a "};
 
-}  // namespace food_chain
+std::vector<std::string> animals = {"fly", "spider", "bird", "cat",
+                                    "dog", "goat",   "cow",  "horse"};
+
+std::vector<std::string> intros = {
+    "",
+    "It wriggled and jiggled and tickled inside her.\n",
+    "How absurd to smallow a bird!\n",
+    "Imagine that, to smallow a dog!\n",
+    "What a hog, to smallow a dog!\n",
+    "Just opened her throat and swallowed a goat!\n",
+    "I don't know how she swallowed a cow!\n",
+    "She's dead, of course!\n"};
+
+std::vector<std::string> endings = {
+    "I don't know why she swallowed the fly. Perhaps she'll die.\n",
+    ".\n",
+    " that wriggled and jiggled and tickled inside her.\n",
+    ".\n",
+    ".\n",
+    ".\n",
+    ".\n"};
+namespace food_chain {
+std::string verse(std::size_t v) {
+  v--;
+  std::string ret = start_intro + animals[v] + ".\n" + intros[v];
+  if (v >= endings.size())
+    return ret;
+  for (int i = v; i > 0; i++) {
+    ret += "She swallowed the " + animals[i] + "to catch the " +
+           animals[i - 1] + endings[i];
+  }
+  ret += endings[0];
+  return ret;
+}
+
+std::string verses(std::size_t start, std::size_t end) {
+  std::string result;
+  for (size_t i = start; i <= end; i++) {
+    result += verse(i) + "\n";
+  }
+  return result;
+}
+std::string sing() { return verses(1, 8); }
+} // namespace food_chain
--- food-chain/food_chain.h
+++ food-chain/food_chain.h
@@ -1,8 +1,13 @@
 #if !defined(FOOD_CHAIN_H)
 #define FOOD_CHAIN_H
+#include <cstddef>
+#include <string>
+#include <vector>
 
 namespace food_chain {
-
-}  // namespace food_chain
+std::string verse(std::size_t v);
+std::string verses(std::size_t start, std::size_t end);
+std::string sing();
+} // namespace food_chain
 
 #endif // FOOD_CHAIN_H
