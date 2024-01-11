59391e4343178d15b4ef2e6a051b2dc09d0b1acf
= <ayoubelmahfoudi05@gmail.com>
2024-01-11 14:53:49 +0100

    Solution submitted, tests passed successfully.

--- roman-numerals/roman_numerals.cpp
+++ roman-numerals/roman_numerals.cpp
@@ -1,5 +1,18 @@
 #include "roman_numerals.h"
 
 namespace roman_numerals {
+std::string convert(int n) {
+  std::stringstream output;
+  for (int i = 0; i < 13; i++) {
+    std::string roman = romans[i];
+    int decimal = decimals[i];
+    int quotient = n / decimal;
+    n %= decimal;
 
-}  // namespace roman_numerals
+    for (int j = 0; j < quotient; ++j) {
+      output << roman;
+    }
+  }
+  return output.str();
+}
+} // namespace roman_numerals
--- roman-numerals/roman_numerals.h
+++ roman-numerals/roman_numerals.h
@@ -1,8 +1,15 @@
 #if !defined(ROMAN_NUMERALS_H)
 #define ROMAN_NUMERALS_H
+#include <sstream>
+#include <string>
 
 namespace roman_numerals {
 
-}  // namespace roman_numerals
+const std::string romans[13] = {"M",  "CM", "D",  "CD", "C",  "XC", "L",
+                                "XL", "X",  "IX", "V",  "IV", "I"};
+const int decimals[13] = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+
+std::string convert(int);
+} // namespace roman_numerals
 
 #endif // ROMAN_NUMERALS_H
