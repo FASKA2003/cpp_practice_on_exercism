99ddb95cd45525d2ab6fc62d1ee743f6dcb99167
= <ayoubelmahfoudi05@gmail.com>
2024-01-12 21:39:12 +0100

    Solution submitted, tests passed successfully.

--- bob/bob.cpp
+++ bob/bob.cpp
@@ -1,5 +1,40 @@
 #include "bob.h"
+#include <cctype>
+#include <string>
 
 namespace bob {
+std::string hey(const std::string &s) {
+  bool question = false;
+  bool upperCase = false;
+  bool lowerCase = false;
+  bool nothing = true;
 
-}  // namespace bob
+  for (char c : s) {
+    if (islower(c))
+      lowerCase = true;
+    if (isupper(c))
+      upperCase = true;
+    if (!isspace(c)) {
+      question = false;
+      nothing = false;
+    }
+    if (c == '?')
+      question = true;
+  }
+  bool shouting = upperCase && !lowerCase;
+  if (shouting && question) {
+    return "Calm down, I know what I'm doing!";
+  }
+  if (shouting) {
+    return "Whoa, chill out!";
+  }
+  if (question) {
+    return "Sure";
+  }
+  if (nothing) {
+    return "Fine. Be that way!";
+  }
+  return "Whatever.";
+}
+
+} // namespace bob
--- bob/bob.h
+++ bob/bob.h
@@ -1,8 +1,9 @@
 #if !defined(BOB_H)
 #define BOB_H
+#include <string>
 
 namespace bob {
-
-}  // namespace bob
+std::string hey(const std::string &s);
+} // namespace bob
 
 #endif // BOB_H
