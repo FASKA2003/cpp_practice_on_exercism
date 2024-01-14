3d5b926d1c41f33345a58469c52a4dc2227326db
= <ayoubelmahfoudi05@gmail.com>
2024-01-14 22:50:25 +0100

    Circular buffer implementation is done.

--- circular-buffer/circular_buffer.cpp
+++ circular-buffer/circular_buffer.cpp
@@ -1,5 +1,33 @@
 #include "circular_buffer.h"
 
 namespace circular_buffer {
-
-}  // namespace circular_buffer
+template <typename T> T circular_buffer<T>::read() {
+  if (_values[_r] == nullptr) {
+    throw std::domain_error("Buffer is empty.");
+  }
+  auto v = *_values[_r];
+  clear();
+  return v;
+}
+template <typename T> void circular_buffer<T>::clear() {
+  if (_values[_r] == nullptr) {
+    return;
+  }
+  _values[_r].reset();
+  increment(_r);
+}
+template <typename T> void circular_buffer<T>::write(T const &v) {
+  if (_values[_w] != nullptr) {
+    throw std::domain_error("Buffer is full.");
+  }
+  _values[_w] = std::make_unique<T>(v);
+  increment(_w);
+}
+template <typename T> void circular_buffer<T>::overwrite(T const &v) {
+  if (_values[_w] != nullptr) {
+    _values[_w].reset();
+    increment(_r);
+  }
+  write(v);
+}
+} // namespace circular_buffer
--- circular-buffer/circular_buffer.h
+++ circular-buffer/circular_buffer.h
@@ -1,8 +1,29 @@
 #if !defined(CIRCULAR_BUFFER_H)
 #define CIRCULAR_BUFFER_H
+#include <memory>
+#include <stdexcept>
 
 namespace circular_buffer {
+template <typename T> class circular_buffer {
+  using t_ptr = std::unique_ptr<T>;
 
-}  // namespace circular_buffer
+private:
+  size_t _size;
+  size_t _r;
+  size_t _w;
+  t_ptr *_values;
+  void increment(size_t &x) { ++x %= _size; }
+
+public:
+  circular_buffer(size_t size)
+      : _size{size}, _r{0}, _w{0}, _values{new t_ptr[size]} {}
+  T read();
+  void write(T const &value);
+  void overwrite(T const &value);
+  void clear();
+  ~circular_buffer() { delete[] _values; }
+};
+
+} // namespace circular_buffer
 
 #endif // CIRCULAR_BUFFER_H
