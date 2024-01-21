0388c5d5ff121ffe7fc1a10d97edd576faeab299
= <ayoubelmahfoudi05@gmail.com>
2024-01-21 18:44:24 +0100

    Solution submitted for the luhn problem.

--- luhn/luhn.cpp
+++ luhn/luhn.cpp
@@ -1,5 +1,24 @@
 #include "luhn.h"
+#include <algorithm>
 
-namespace luhn {
+bool luhn::valid(std::string number) {
+  number.erase(remove_if(number.begin(), number.end(), isspace), number.end());
+  if (number == "0") {
+    return false;
+  }
+  std::reverse(number.begin(), number.end());
+  int counter = 0;
+  int sum = 0;
+  for (char &digit : number) {
+    digit = digit - '0';
+    if (digit < 0 || digit > 9)
+      return false;
+    if (counter % 2 != 0) {
+      digit = 2 * digit - 9 * (digit / 5);
+    }
+    counter = counter + 1;
+    sum = sum + digit;
+  }
 
-}  // namespace luhn
+  return sum % 10 == 0;
+}
