63ff750f38c91ddd9a6cd6c0305b9c8cd3d0b15c
= <ayoubelmahfoudi05@gmail.com>
2024-01-24 19:20:55 +0100

    Solution submitted, tests passed successfully.

--- armstrong-numbers/armstrong_numbers.cpp
+++ armstrong-numbers/armstrong_numbers.cpp
@@ -1,5 +1,16 @@
 #include "armstrong_numbers.h"
 
 namespace armstrong_numbers {
+bool is_armstrong_number(int num) {
+  int sum = 0;
+  std::ostringstream os;
 
-}  // namespace armstrong_numbers
+  os << num;
+  std::string digits = os.str();
+  for (char ch : digits) {
+    int value = ch - '0';
+    sum += pow(value, (int)digits.length());
+  }
+  return (sum == num);
+}
+} // namespace armstrong_numbers
--- armstrong-numbers/armstrong_numbers.h
+++ armstrong-numbers/armstrong_numbers.h
@@ -1,8 +1,11 @@
 #if !defined(ARMSTRONG_NUMBERS_H)
 #define ARMSTRONG_NUMBERS_H
+#include <cmath>
+#include <string>
+#include <sstream>
 
 namespace armstrong_numbers {
-
-}  // namespace armstrong_numbers
+bool is_armstrong_number(int num);
+} // namespace armstrong_numbers
 
 #endif // ARMSTRONG_NUMBERS_H
