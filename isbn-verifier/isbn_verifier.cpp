4e844370e9a26ae34980f18c1caf1747be0123a8
= <ayoubelmahfoudi05@gmail.com>
2024-02-03 13:02:43 +0100

    isbn-verifier: solution submitted, tests passed successfully.

--- isbn-verifier/isbn_verifier.cpp
+++ isbn-verifier/isbn_verifier.cpp
@@ -1,5 +1,30 @@
 #include "isbn_verifier.h"
+#include <cctype>
 
 namespace isbn_verifier {
-
-}  // namespace isbn_verifier
+bool is_valid(std::string input) {
+  int sum = 0, counter = 10, value;
+  bool dash = false;
+  for (char ch : input) {
+    if (ch == 45) {
+      dash = true;
+    }
+    if (std::isdigit(ch)) {
+      value = ch - '0';
+      sum += value * counter;
+      counter--;
+    }
+  }
+  value = input.length();
+  if (dash && (value != 13))
+    return false;
+  if (!dash && (value != 10))
+    return false;
+  if (counter == 1) {
+    if (input[value - 1] != 'X')
+      return false;
+    sum += 10;
+  }
+  return ((sum % 11) == 0);
+}
+} // namespace isbn_verifier
--- isbn-verifier/isbn_verifier.h
+++ isbn-verifier/isbn_verifier.h
@@ -1,8 +1,9 @@
 #if !defined(ISBN_VERIFIER_H)
 #define ISBN_VERIFIER_H
+#include <string>
 
 namespace isbn_verifier {
-    
+bool is_valid(std::string input);
 } // namespace isbn_verifier
 
 #endif // ISBN_VERIFIER_H
