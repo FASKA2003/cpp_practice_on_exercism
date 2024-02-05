02c79603f7a60c8e42c4e161f5d5cab1f2feb350
= <ayoubelmahfoudi05@gmail.com>
2024-02-05 16:53:05 +0100

    diamond problem : To be continued tomorrow.

--- diamond/diamond.cpp
+++ diamond/diamond.cpp
@@ -1,5 +1,14 @@
 #include "diamond.h"
 
 namespace diamond {
-
+  std::vector<std::string> rows(char letter) {
+    int Index1 = (letter - 'A') , Index2 = Index1;
+    int size = 2 * Index1 + 1;
+    std::vector<std::string> diamond (size, std::string(size, ' '));
+    for (int row = 0; row < size; row++) {
+      for (int col = 0 ; col < size; col ++) { 
+      }
+    }
+    return diamond;
+  }
 }  // namespace diamond
--- diamond/diamond.h
+++ diamond/diamond.h
@@ -1,5 +1,7 @@
 #pragma once
+#include <string>
+#include <vector>
 
 namespace diamond {
-
-}  // namespace diamond
+std::vector<std::string> rows(char letter);
+} // namespace diamond
