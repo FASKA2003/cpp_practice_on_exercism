672490551ad2f478e9a3e7abee4ba12b74877cde
= <ayoubelmahfoudi05@gmail.com>
2024-02-06 09:54:06 +0100

    Diamond Problem : Solution submitted, tests passed successfuly.

--- diamond/diamond.cpp
+++ diamond/diamond.cpp
@@ -1,14 +1,20 @@
 #include "diamond.h"
 
 namespace diamond {
-  std::vector<std::string> rows(char letter) {
-    int Index1 = (letter - 'A') , Index2 = Index1;
-    int size = 2 * Index1 + 1;
-    std::vector<std::string> diamond (size, std::string(size, ' '));
-    for (int row = 0; row < size; row++) {
-      for (int col = 0 ; col < size; col ++) { 
-      }
-    }
-    return diamond;
+std::vector<std::string> rows(char letter) {
+  int Index1 = (letter - 'A');
+  int size = 2 * Index1 + 1;
+  char nextChar = 'A';
+  std::vector<std::string> diamond{};
+  for (int i = Index1; i >= 0; i--) {
+    std::string temp(size, ' ');
+    temp[i] = temp[size - i - 1] = nextChar;
+    nextChar++;
+    diamond.push_back(temp);
   }
-}  // namespace diamond
+  for (int i = Index1 - 1; i >= 0; i--) {
+    diamond.push_back(diamond.at(i));
+  }
+  return diamond;
+}
+} // namespace diamond
