51b5386673578c0c4038523e5710f64447506037
= <ayoubelmahfoudi05@gmail.com>
2024-02-08 00:00:13 +0100

    run-length-encoding: solution submitted, tests passed successfully.

--- run-length-encoding/run_length_encoding.cpp
+++ run-length-encoding/run_length_encoding.cpp
@@ -1,5 +1,47 @@
 #include "run_length_encoding.h"
+#include <cctype>
+#include <sstream>
+#include <string>
 
 namespace run_length_encoding {
-    
-}  // namespace run_length_encoding
+
+std::string encode(std::string data) {
+  if (data.empty())
+    return "";
+
+  std::stringstream encoded_data;
+  int count = 1;
+  for (size_t i = 1; i <= data.size(); ++i) {
+    if (i < data.size() && data[i] == data[i - 1]) {
+      ++count;
+    } else {
+      if (count > 1) {
+        encoded_data << count;
+      }
+      encoded_data << data[i - 1];
+      count = 1;
+    }
+  }
+  return encoded_data.str();
+}
+
+std::string decode(std::string data) {
+  if (data.empty())
+    return "";
+
+  std::stringstream decoded_data;
+  std::string num = "";
+
+  for (size_t i = 0; i < data.size(); ++i) {
+    if (isdigit(data[i])) {
+      num += data[i];
+    } else {
+      int count = num.empty() ? 1 : std::stoi(num);
+      decoded_data << std::string(count, data[i]);
+      num = "";
+    }
+  }
+  return decoded_data.str();
+}
+
+} // namespace run_length_encoding
--- run-length-encoding/run_length_encoding.h
+++ run-length-encoding/run_length_encoding.h
@@ -1,5 +1,7 @@
 #pragma once
-
+#include <string>
 namespace run_length_encoding {
+std::string encode(std::string data);
+std::string decode(std::string data);
 
-}  // namespace run_length_encoding
+} // namespace run_length_encoding
