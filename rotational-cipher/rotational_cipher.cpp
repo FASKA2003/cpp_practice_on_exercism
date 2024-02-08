d9074a09d12527ff3bb4388b57436620d52adc6b
= <ayoubelmahfoudi05@gmail.com>
2024-02-08 12:17:00 +0100

    rotational-cipher : solution submitted, tests passed successfully.

--- rotational-cipher/rotational_cipher.cpp
+++ rotational-cipher/rotational_cipher.cpp
@@ -1,5 +1,18 @@
 #include "rotational_cipher.h"
+#include <cctype>
 
 namespace rotational_cipher {
-
-}  // namespace rotational_cipher
+std::string rotate(std::string plain, int key) {
+  std::string cipher;
+  int offset;
+  for (char ch : plain) {
+    if (std::isalpha(ch)) {
+      offset = std::islower(ch) ? 97 : 65;
+      cipher += (char)((ch - offset + key) % 26 + offset);
+    } else {
+      cipher += (char)ch;
+    }
+  }
+  return cipher;
+}
+} // namespace rotational_cipher
--- rotational-cipher/rotational_cipher.h
+++ rotational-cipher/rotational_cipher.h
@@ -1,7 +1,6 @@
 #pragma once
-
 #include <string>
 
 namespace rotational_cipher {
-
-}  // namespace rotational_cipher
+std::string rotate(std::string plain, int key);
+} // namespace rotational_cipher
