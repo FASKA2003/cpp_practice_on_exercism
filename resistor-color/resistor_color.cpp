fec3394d945560574efa27f30bce6fdf9d9d5408
= <ayoubelmahfoudi05@gmail.com>
2024-02-09 12:53:07 +0100

    resistor problem : solution submitted, tests passed successfully.

--- resistor-color/resistor_color.cpp
+++ resistor-color/resistor_color.cpp
@@ -1,5 +1,17 @@
 #include "resistor_color.h"
+#include <map>
+
+std::map<std::string, int> Cmap = {
+    {"black", 0}, {"brown", 1}, {"red", 2},    {"orange", 3}, {"yellow", 4},
+    {"green", 5}, {"blue", 6},  {"violet", 7}, {"grey", 8},   {"white", 9}};
 
 namespace resistor_color {
-
-}  // namespace resistor_color
+int color_code(std::string color) { return Cmap[color]; }
+std::vector<std::string> colors() {
+  std::vector<std::string> result = {
+      "black", "brown", "red",    "orange", "yellow",
+      "green", "blue",  "violet", "grey",   "white",
+  };
+  return result;
+}
+} // namespace resistor_color
--- resistor-color/resistor_color.h
+++ resistor-color/resistor_color.h
@@ -1,5 +1,8 @@
 #pragma once
+#include <string>
+#include <vector>
 
 namespace resistor_color {
-
-}  // namespace resistor_color
+int color_code(std::string color);
+std::vector<std::string> colors();
+} // namespace resistor_color
