ffa905f92d10a586702d0ef9d5e1235ca7d61766
= <ayoubelmahfoudi05@gmail.com>
2024-02-10 12:58:55 +0100

    resistor-color-duo : Solution submitted, tests passed successfully.

--- resistor-color-duo/resistor_color_duo.cpp
+++ resistor-color-duo/resistor_color_duo.cpp
@@ -1,5 +1,17 @@
 #include "resistor_color_duo.h"
+#include <sstream>
+
+std::map<std::string, int> Cmap = {
+    {"black", 0}, {"brown", 1}, {"red", 2},    {"orange", 3}, {"yellow", 4},
+    {"green", 5}, {"blue", 6},  {"violet", 7}, {"grey", 8},   {"white", 9}};
 
 namespace resistor_color_duo {
-
-}  // namespace resistor_color_duo
+int value(std::vector<std::string> input) {
+  std::stringstream result;
+  int returnVal;
+  result << Cmap[input[0]];
+  result << Cmap[input[1]];
+  result >> returnVal;
+  return returnVal;
+}
+} // namespace resistor_color_duo
--- resistor-color-duo/resistor_color_duo.h
+++ resistor-color-duo/resistor_color_duo.h
@@ -1,5 +1,8 @@
 #pragma once
+#include <map>
+#include <string>
+#include <vector>
 
 namespace resistor_color_duo {
-
-}  // namespace resistor_color_duo
+int value(std::vector<std::string> input);
+} // namespace resistor_color_duo
